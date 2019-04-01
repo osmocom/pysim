45daa925bd950b6d1ebc791e9a0e8f7b70f64c1b
Philipp Maier <pmaier@sysmocom.de>
2019-04-01 15:49:45 +0200

    cosmetic: fix sourecode formatting
    Change-Id: I4836826811ffb0aeb103d32eb874f5fa52af4186

--- pySim/cards.py
+++ pySim/cards.py
@@ -338,12 +338,12 @@ class FakeMagicSim(Card):
 
 		# Set first entry
 		entry = (
-			'81' +								#  1b  Status: Valid & Active
+			'81' +					#  1b  Status: Valid & Active
 			rpad(b2h(p['name'][0:14]), 28) +	# 14b  Entry Name
-			enc_iccid(p['iccid']) +				# 10b  ICCID
-			enc_imsi(p['imsi']) +				#  9b  IMSI_len + id_type(9) + IMSI
-			p['ki'] +							# 16b  Ki
-			lpad(p['smsp'], 80)					# 40b  SMSP (padded with ff if needed)
+			enc_iccid(p['iccid']) +			# 10b  ICCID
+			enc_imsi(p['imsi']) +			#  9b  IMSI_len + id_type(9) + IMSI
+			p['ki'] +				# 16b  Ki
+			lpad(p['smsp'], 80)			# 40b  SMSP (padded with ff if needed)
 		)
 		self._scc.update_record('000c', 1, entry)
 
