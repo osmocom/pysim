8b72c27270c5582e92a3a3fd7e04b6bf7da9fe40
Supreeth Herle <herlesupreeth@gmail.com>
2020-03-22 08:55:50 +0100

    Define mapping between ISIM Service Number and its description
    This commit introduces a lookup table which maps ISIM Service Number to its description.
    The mapping is defined in 3GPP TS 31.103 version 14.2.0 Release 14, 4.2.7 EF.IST (ISIM Service Table)
    
    Change-Id: Iad51d0804259df47729308b461062f794b135e66

---
+++ pySim/ts_31_103.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Various constants from ETSI TS 131 103 V14.2.0
+"""
+
+#
+# Copyright (C) 2020 Supreeth Herle <herlesupreeth@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Mapping between ISIM Service Number and its description
+EF_IST_map = {
+	1: 'P-CSCF address',
+	2: 'Generic Bootstrapping Architecture (GBA)',
+	3: 'HTTP Digest',
+	4: 'GBA-based Local Key Establishment Mechanism',
+	5: 'Support of P-CSCF discovery for IMS Local Break Out',
+	6: 'Short Message Storage (SMS)',
+	7: 'Short Message Status Reports (SMSR)',
+	8: 'Support for SM-over-IP including data download via SMS-PP as defined in TS 31.111 [31]',
+	9: 'Communication Control for IMS by ISIM',
+	10: 'Support of UICC access to IMS',
+	11: 'URI support by UICC',
+	12: 'Media Type support',
+	13: 'IMS call disconnection cause',
+	14: 'URI support for MO SHORT MESSAGE CONTROL',
+	15: 'MCPTT',
+	16: 'URI support for SMS-PP DOWNLOAD as defined in 3GPP TS 31.111 [31]',
+	17: 'From Preferred',
+	18: 'IMS configuration data',
+	19: 'XCAP Configuration Data'
+}
