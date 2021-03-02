samkenxstream/SamKenX-brain-network

move SW matching to a generic utility function
This will allow using it outside the transport/__init__.py

Change-Id: Id26dfefa85d91e3b3a23e0049f3b833e29cb1cef

pySim/utils.py
@@ -756,3 +756,17 @@ def get_addr_type(addr):
 		return 0x00
 
 	return None
+
+def sw_match(sw, pattern):
+	"""Match given SW against given pattern."""
+	# Create a masked version of the returned status word
+	sw_lower = sw.lower()
+	sw_masked = ""
+	for i in range(0, 4):
+		if sw_lower[i] == '?':
+			sw_masked = sw_masked + '?'
+		elif sw_lower[i] == 'x':
+			sw_masked = sw_masked + 'x'
+		else:
+			sw_masked = sw_masked + sw_lower[i]
+	return sw == pattern

osmocom/pysim

pySim/utils.py
@@ -759,3 +759,17 @@ def get_addr_type(addr):
 		return 0x00
 
 	return None
+
+def sw_match(sw, pattern):
+	"""Match given SW against given pattern."""
+	# Create a masked version of the returned status word
+	sw_lower = sw.lower()
+	sw_masked = ""
+	for i in range(0, 4):
+		if sw_lower[i] == '?':
+			sw_masked = sw_masked + '?'
+		elif sw_lower[i] == 'x':
+			sw_masked = sw_masked + 'x'
+		else:
+			sw_masked = sw_masked + sw_lower[i]
+	return sw_masked == pattern