ad073e834ae4bd2574634628dd2af7e308c2d143
Philipp Maier <pmaier@sysmocom.de>
2021-03-10 12:07:48 +0100

    ts_31_102: do not add empty ShellCommands class.
    The class ShellCommands defined in ADF_USIM overloads useful CommandSet
    classes defined in the superclass, making their commands inaccessible.
    Also ts_31_102 does not have such a class definition in the ADF_ISIM
    class, so lets remove this class.
    
    Change-Id: I0e67c570fc4f17641d990a9cd239632ecf622de3
    Related: OS#4963

--- pySim/ts_31_102.py
+++ pySim/ts_31_102.py
@@ -486,7 +486,6 @@ class ADF_USIM(CardADF):
     def __init__(self, aid='a0000000871002', name='ADF.USIM', fid=None, sfid=None,
                  desc='USIM Application'):
         super().__init__(aid=aid, fid=fid, sfid=sfid, name=name, desc=desc)
-        self.shell_commands = [self.ShellCommands()]
 
         files = [
           EF_LI(sfid=0x02),
@@ -517,12 +516,6 @@ class ADF_USIM(CardADF):
 
     def decode_select_response(self, data_hex):
         return pySim.ts_102_221.decode_select_response(data_hex)
-
-    @with_default_category('File-Specific Commands')
-    class ShellCommands(CommandSet):
-        def __init__(self):
-            super().__init__()
-
 
 # TS 31.102 Section 7.3
 sw_usim = {
