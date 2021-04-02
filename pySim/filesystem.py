5a4fd529869573f20b88157333872d39da56bd45
Harald Welte <laforge@osmocom.org>
2021-04-02 16:05:26 +0200

    filesystem: Avoid GPL header showing up in sphinx autodoc
    Change-Id: I1d963ae3d1511ef40d1ebcb36b0f67c40cbd6309

--- pySim/filesystem.py
+++ pySim/filesystem.py
@@ -7,22 +7,22 @@ entries, further sub-divided into the EF sub-types Transparent, Linear Fixed, et
 
 The classes are intended to represent the *specification* of the filesystem,
 not the actual contents / runtime state of interacting with a given smart card.
-
-(C) 2021 by Harald Welte <laforge@osmocom.org>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
+# (C) 2021 by Harald Welte <laforge@osmocom.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import code
 import json
