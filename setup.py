9813dc958b05fb86c8c2f3d28df7bde2f4a2d309
Harald Welte <laforge@osmocom.org>
2021-04-03 15:13:13 +0200

    Introduce setuptools support
    Add a pyproject.toml and setup.py for using setuptools to install
    pySim and its upstream dependencies.
    
    Change-Id: I5698f3b29184340db69a156f985aa3c78d9b5674

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='pySim',
+    version='1.0',
+    packages=['pySim'],
+    url='https://osmocom.org/projects/pysim/wiki',
+    license='GPLv2',
+    author_email='simtrace@lists.osmocom.org',
+    description='Tools related to SIM/USIM/ISIM cards',
+    install_requires=[
+        "pyscard",
+        "serial",
+        "pytlv",
+        "cmd2"
+    ],
+    scripts=[
+        'pySim-prog.py',
+        'pySim-read.py',
+        'pySim-shell.py'
+    ]
+)
