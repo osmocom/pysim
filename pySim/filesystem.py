0e4515f53d3aaa27e3f56058145b833f17547089
Philipp Maier <pmaier@sysmocom.de>
2022-01-04 18:04:44 +0100

    filesystem: use correct AID for applications found by probing
    When printing applications found by probing for a specific AID, then the
    wrong variable is used to print the AID.
    
    Change-Id: I3d5ec28e46fe00c0d793a1d9ef0a0e0900649a4d

--- pySim/filesystem.py
+++ pySim/filesystem.py
@@ -1115,7 +1115,7 @@ class RuntimeState(object):
         for f in set(apps_profile) - set(apps_taken):
             data, sw = self.card.select_adf_by_aid(f.aid)
             if sw == "9000":
-                print(" %s: %s" % (f.name, a))
+                print(" %s: %s" % (f.name, f.aid))
                 apps_taken.append(f)
 
         return apps_taken
