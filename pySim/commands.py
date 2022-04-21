9970f59f4fd0a51da2b2e3a8194e6d6af0468581
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2022-04-22 00:29:10 +0300

    SimCardCommands.run_gsm(): use send_apdu_checksw()
    Change-Id: Ib713cf8154a3aba72bc5776a8d99ec47631ade28

--- pySim/commands.py
+++ pySim/commands.py
@@ -407,7 +407,7 @@ class SimCardCommands:
         if len(rand) != 32:
             raise ValueError('Invalid rand')
         self.select_path(['3f00', '7f20'])
-        return self._tp.send_apdu('a0' + '88000010' + rand)
+        return self._tp.send_apdu_checksw('a0' + '88000010' + rand, sw='9000')
 
     def authenticate(self, rand: str, autn: str, context='3g'):
         """Execute AUTHENTICATE (USIM/ISIM).
