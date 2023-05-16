ade366d2a9bc319d5f353f648b9e7f390de80715
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2023-05-16 17:08:57 +0700

    setup.py: add missing packages for pySim-trace.py
    pySim-trace.py is broken if pySim is installed using setup.py:
    
      fixeria@DELL:~$ pySim-trace.py
      Traceback (most recent call last):
        File "/usr/bin/pySim-trace.py", line 8, in <module>
          from pySim.apdu import *
      ModuleNotFoundError: No module named 'pySim.apdu'
    
    Change-Id: I371143cb4009db46275ec7a020497b909dcc3b4e

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pySim',
     version='1.0',
-    packages=['pySim', 'pySim.transport'],
+    packages=['pySim', 'pySim.transport', 'pySim.apdu', 'pySim.apdu_source'],
     url='https://osmocom.org/projects/pysim/wiki',
     license='GPLv2',
     author_email='simtrace@lists.osmocom.org',
