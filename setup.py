osmocom/pysim

commit 2b15e315e2d2b573787d9bd99f84c61086683547

setup: add missing pyyaml to setup.py and README.md
Change-Id: I1d35f38b17a315dd58e8dd91a27bfa6c2c85905d

setup.py
@@ -17,6 +17,7 @@ setup(
         "construct >= 2.9.51",
         "bidict",
         "gsm0338",
+        "pyyaml >= 5.1"
         "termcolor",
         "colorlog",
         "pycryptodome"

commit bca01523dfe0105c3af7e7b1602fcfd2e45fd492

setup.py: fix syntax errors (missing commas)
Change-Id: Ia53a659ad9652d582e2bf4a039a3e18631435072
Fixes: 2b15e315 "setup: add missing pyyaml to setup.py and README.md"
Fixes: 93aac3ab "pySim-shell: fix compatibility problem with cmd2 >= 2.0.0 (Settable)"

setup.py
@@ -17,11 +17,11 @@ setup(
         "construct >= 2.9.51",
         "bidict",
         "gsm0338",
-        "pyyaml >= 5.1"
+        "pyyaml >= 5.1",
         "termcolor",
         "colorlog",
-        "pycryptodome"
-        "packaging"
+        "pycryptodome",
+        "packaging",
     ],
     scripts=[
         'pySim-prog.py',