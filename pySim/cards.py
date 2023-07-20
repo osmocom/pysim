3175d61eb2746cb7c23f08cc3a6b3031bf5d2413
Philipp Maier <pmaier@sysmocom.de>
2023-07-20 17:28:10 +0200

    cards: fix swapped PIN mapping number
    The constant for _adm_chv_num is swapped. It should be 0x0A, rather than
    0xA0
    
    Change-Id: I5680d2deee855ef316a98058e8c8ff8cf4edbbb2

--- pySim/cards.py
+++ pySim/cards.py
@@ -101,7 +101,7 @@ class UiccCardBase(SimCardBase):
         self._scc.cla_byte = "00"
         self._scc.sel_ctrl = "0004"  # request an FCP
         # See also: ETSI TS 102 221, Table 9.3
-        self._adm_chv_num = 0xA0
+        self._adm_chv_num = 0x0A
 
     def probe(self) -> bool:
         # EF.DIR is a mandatory EF on all ICCIDs; however it *may* also exist on a TS 51.011 SIM
