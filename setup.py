e6f3e153b5cdba7ea51cab91f3ee880dc71cf31c
Harald Welte <laforge@osmocom.org>
2024-02-07 16:23:35 +0100

    setup.py: Expose pySim.esim as package
    Change-Id: I524d2b160e743e9a75d08d3bb285ed5781e65c59

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='pySim',
     version='1.0',
-    packages=['pySim', 'pySim.legacy', 'pySim.transport', 'pySim.apdu', 'pySim.apdu_source'],
+    packages=['pySim', 'pySim.legacy', 'pySim.transport', 'pySim.apdu', 'pySim.apdu_source',
+              'pySim.esim'],
     url='https://osmocom.org/projects/pysim/wiki',
     license='GPLv2',
     author_email='simtrace@lists.osmocom.org',
