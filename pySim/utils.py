76e18254499aff27dea07c93e14eb7f74426240f
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2024-02-28 14:57:33 +0100

    add try-except for handling malformed imsi

--- pySim/utils.py
+++ pySim/utils.py
@@ -422,21 +422,25 @@ def enc_imsi(imsi: str):
 
 def dec_imsi(ef: Hexstr) -> Optional[str]:
     """Converts an EF value to the IMSI string representation"""
-    if len(ef) < 4:
+    try:
+        if len(ef) < 4:
+            return None
+        l = int(ef[0:2], 16) * 2		# Length of the IMSI string
+        l = l - 1						# Encoded length byte includes oe nibble
+        swapped = swap_nibbles(ef[2:]).rstrip('f')
+        if len(swapped) < 1:
+            return None
+        oe = (int(swapped[0]) >> 3) & 1  # Odd (1) / Even (0)
+        if not oe:
+            # if even, only half of last byte was used
+            l = l-1
+        if l != len(swapped) - 1:
+            return None
+        imsi = swapped[1:]
+        return imsi
+    except ValueError as err:
+        # print("IMSI is present but malformed")
         return None
-    l = int(ef[0:2], 16) * 2		# Length of the IMSI string
-    l = l - 1						# Encoded length byte includes oe nibble
-    swapped = swap_nibbles(ef[2:]).rstrip('f')
-    if len(swapped) < 1:
-        return None
-    oe = (int(swapped[0]) >> 3) & 1  # Odd (1) / Even (0)
-    if not oe:
-        # if even, only half of last byte was used
-        l = l-1
-    if l != len(swapped) - 1:
-        return None
-    imsi = swapped[1:]
-    return imsi
 
 
 def dec_iccid(ef: Hexstr) -> str:
