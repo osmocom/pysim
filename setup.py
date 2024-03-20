479aeb0b00fdb96a5dc2a92db962c84146f53884
Cody Harris <osmocom@hypodyne.net>
2024-03-20 14:28:00 -0700

    add missing modules to setup.py
    Change-Id: I330d5e35e5f1b508c6209b6894009b5fdd35d660

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from setuptools import setup
 setup(
     name='pySim',
     version='1.0',
-    packages=['pySim', 'pySim.legacy', 'pySim.transport', 'pySim.apdu', 'pySim.apdu_source'],
+    packages=[
+        'pySim',
+        'pySim.apdu',
+        'pySim.apdu_source',
+        'pySim.esim',
+        'pySim.global_platform',
+        'pySim.legacy',
+        'pySim.transport',
+    ],
     url='https://osmocom.org/projects/pysim/wiki',
     license='GPLv2',
     author_email='simtrace@lists.osmocom.org',
