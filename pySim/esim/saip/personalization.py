d5d776b0032cd5aa67945d032bb5ff86b6ea75c2
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2025-03-01 20:09:33 +0100

    esim.saip: implement batch personalization
    Implement BatchPersonalization, generating N eSIM profiles from a preset
    configuration.
    
    Batch parameters can be fed by a constant, incrementing, random or from
    CSV rows: add param_source.py with classes to feed such input to each of
    the BatchPersonalization's ConfigurableParameter instances.
    
    Related: SYS#6768
    Change-Id: I497c60c101ea0eea980e8b1a4b1f36c0eda39002

--- pySim/esim/saip/personalization.py
+++ pySim/esim/saip/personalization.py
@@ -16,12 +16,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import io
-from typing import List, Tuple, Optional
+import copy
+from typing import List, Tuple, Optional, Generator
 
 from osmocom.tlv import camel_to_snake
 from osmocom.utils import hexstr
-from pySim.utils import enc_iccid, dec_iccid, enc_imsi, dec_imsi, h2b, b2h, rpad, sanitize_iccid
+from pySim.utils import enc_iccid, dec_iccid, enc_imsi, dec_imsi, h2b, b2h, rpad, sanitize_iccid, all_subclasses_of
 from pySim.esim.saip import ProfileElement, ProfileElementSequence
+from pySim.esim.saip import param_source
 
 def unrpad(s: hexstr, c='f') -> hexstr:
     return hexstr(s.rstrip(c))
@@ -52,11 +54,6 @@ def file_tuples_content_as_bytes(l: List[Tuple]) -> Optional[bytes]:
         else:
             return ValueError("Unknown key '%s' in tuple list" % k)
     return stream.getvalue()
-
-def all_subclasses_of(cls):
-    for subc in cls.__subclasses__():
-        yield subc
-        yield from all_subclasses_of(subc)
 
 
 class ConfigurableParameter:
@@ -668,3 +665,63 @@ class Opc(K):
             if algoConfiguration[0] != 'algoParameter':
                 continue
             yield algoConfiguration[1][cls.key]
+
+
+class BatchPersonalization:
+
+    class ParamAndSrc:
+        'tie a ConfigurableParameter to a source of actual values'
+        def __init__(self, param:ConfigurableParameter, src:param_source.ParamSource):
+            self.param = param
+            self.src = src
+
+    def __init__(self,
+                 n:int,
+                 src_pes:ProfileElementSequence,
+                 params:list[ParamAndSrc]=None,
+                 csv_rows:Generator=None,
+                ):
+        self.n = n
+        self.params = params or []
+        self.src_pes = src_pes
+        self.csv_rows = csv_rows
+
+    def add_param_and_src(self, param:ConfigurableParameter, src:param_source.ParamSource):
+        self.params.append(BatchPersonalization.ParamAndSrc(param=param, src=src))
+
+    def generate_profiles(self):
+        # get first row of CSV: column names
+        csv_columns = None
+        if self.csv_rows:
+            try:
+                csv_columns = next(self.csv_rows)
+            except StopIteration as e:
+                raise ValueError('the input CSV file appears to be empty') from e
+
+        for i in range(self.n):
+            csv_row = None
+            if self.csv_rows and csv_columns:
+                try:
+                    csv_row_list = next(self.csv_rows)
+                except StopIteration as e:
+                    raise ValueError(f'not enough rows in the input CSV for eSIM nr {i+1} of {self.n}') from e
+
+                csv_row = dict(zip(csv_columns, csv_row_list))
+
+            pes = copy.deepcopy(self.src_pes)
+
+            for p in self.params:
+                try:
+                    input_value = p.src.get_next(csv_row=csv_row)
+                    assert input_value is not None
+                    value = p.param.__class__.validate_val(input_value)
+                    p.param.__class__.apply_val(pes, value)
+                except (
+                        TypeError,
+                        ValueError,
+                        KeyError,
+                       ) as e:
+                    raise ValueError(f'{p.param.name} fed by {p.src.name}: {e}'
+                                     f' (input_value={p.param.input_value!r} value={p.param.value!r})') from e
+
+            yield pes
