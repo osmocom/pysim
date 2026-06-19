925573f4d7a1754416df881eaeff0c6005eea1e1
Neels Hofmeyr <nhofmeyr@sysmocom.de>
2026-06-19 03:47:26 +0200

    test_configurable_parameters: test less templates
    Change-Id: Ib75b6919a3acfddd99bf9baa9b6847ef731b9e67
    Jenkins: skip-card-test

--- tests/unittests/test_configurable_parameters.py
+++ tests/unittests/test_configurable_parameters.py
@@ -55,8 +55,6 @@ class ConfigurableParameterTest(unittest.TestCase):
         upp_fnames = (
                 'TS48v5_SAIP2.1A_NoBERTLV.der',
                 'TS48v5_SAIP2.3_BERTLV_SUCI.der',
-                'TS48v5_SAIP2.1B_NoBERTLV.der',
-                'TS48v5_SAIP2.3_NoBERTLV.der',
                )
 
         class Paramtest:
